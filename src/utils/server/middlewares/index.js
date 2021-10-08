dd6cba15a6314cde3ff7919c3e05481965751f38
ahmdGeek <ahmadalymohamad@gmail.com>
2021-10-08 17:15:42 +0200

    admin editor checker middleware export

--- src/utils/server/middlewares/index.js
+++ src/utils/server/middlewares/index.js
@@ -4,3 +4,4 @@ export * from './requireAuth';
 export * from './requireSubscription';
 export * from './parseCourseSubmission';
 export * from './requireAdminRole';
+export * from './requireAdminEditorRole';
