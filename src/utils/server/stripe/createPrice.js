71ae39f35b82c90fb9337fc90efc82f6b9c79a49
ahmdGeek <ahmadalymohamad@gmail.com>
2021-10-11 21:22:28 +0200

    ref, create new price with right decimal point

--- src/utils/server/stripe/createPrice.js
+++ src/utils/server/stripe/createPrice.js
@@ -1,7 +1,10 @@
-const createPrice = ({ pId, amount = 0 } = {}) => {
-  const price = stripe.prices.create({
+import { getStripe } from './getStripe';
+
+const stripe = getStripe();
+const createPrice = async ({ pId, amount = 0 } = {}) => {
+  const price = await stripe.prices.create({
     product: pId,
-    unit_amount: amount * 1000,
+    unit_amount: amount * 100,
     currency: 'usd',
   });
   return {
