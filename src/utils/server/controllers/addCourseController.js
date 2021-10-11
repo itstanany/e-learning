2cb4dd60d2edd3d7296397696b56061f5a63958f
ahmdGeek <ahmadalymohamad@gmail.com>
2021-10-11 21:21:05 +0200

    create stripe product and price for new course

--- src/utils/server/controllers/addCourseController.js
+++ src/utils/server/controllers/addCourseController.js
@@ -1,4 +1,6 @@
+import { AddProductAndPriceId } from '../addProductAndPriceId';
 import { saveCourseInfo } from '../saveCourseInfo';
+import { createPrice, createProduct } from '../stripe';
 
 const addCourseController = async (req, res) => {
   try {
@@ -9,13 +11,31 @@ const addCourseController = async (req, res) => {
       ...courseInfo,
     };
     if (fileLocation) parsedCourseInfo.thumbnail = fileLocation;
-    const { saved/* , courseId */ } = await saveCourseInfo({
+    const { saved, courseId } = await saveCourseInfo({
       courseInfo: parsedCourseInfo,
       lectures,
     });
+
+    const { product } = await createProduct({
+      id: courseId,
+      name: courseInfo?.title,
+      images: fileLocation ? [fileLocation] : [],
+    });
+
+    const { price } = await createPrice({
+      pId: product?.id,
+      amount: courseInfo?.price,
+    });
+
+    await AddProductAndPriceId({
+      courseId,
+      productId: product?.id,
+      priceId: price?.id,
+    });
+
     return res.json({ submitted: saved });
   } catch (error) {
-    return res.statusCode(400).json({ error: `Error during saving the course. ${error.message}` });
+    return res.status(400).json({ error: `Error during saving the course. ${error.message}` });
   }
 };
 
