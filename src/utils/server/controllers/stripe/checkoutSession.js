28b57f9d312077c4386642b38b65f9c6be5db7d0
Ahmed Ali Mohamed <AhmadAlyMohamad@gmail.com>
2021-10-15 11:25:25 +0200

    custom success and cancel page per course

--- src/utils/server/controllers/stripe/checkoutSession.js
+++ src/utils/server/controllers/stripe/checkoutSession.js
@@ -8,17 +8,15 @@ const stripe = getStripe();
 
 const checkoutSessionController = async (req, res) => {
   if (req.method === 'POST') {
-    console.log('inside post method ahndler')
     try {
       const session = await stripe.checkout.sessions.create({
         mode: 'payment',
         payment_method_types: ['card'],
         line_items: req?.body?.items ?? [],
         // eslint-disable-next-line no-undef
-        success_url: `${req.headers.origin}/success?session_id={CHECKOUT_SESSION_ID}`,
-        cancel_url: `${req.headers.origin}/cancel`,
+        success_url: `${req.headers.referer}/success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${req.headers.referer}/cancel`,
       });
-      console.log({ session });
       return res.status(200).json(session);
     } catch (err) {
       return res.status(500).json({
