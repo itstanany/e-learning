416b32d19a478c1623849c40aaa5e5249f110861
Ahmed Ali Mohamed <AhmadAlyMohamad@gmail.com>
2021-10-18 16:36:12 +0200

    style, shadow of landing view

--- src/components/HomePage/styles/homeStyle.js
+++ src/components/HomePage/styles/homeStyle.js
@@ -1,6 +1,19 @@
 const homeStyle = {
   landingView: {
     height: 'calc(100vh - 65px)',
+    '&::after': {
+      content: '""',
+      position: 'absolute',
+      backgroundColor: 'rgba(0, 0, 0, 0.4)',
+      backgroundImage: `linear-gradient(to top,
+        rgba(0, 0, 0, 0.8),
+        rgba(0, 0, 0, 0) 60%,
+        rgba(0, 0, 0, 0.8) 100%)`,
+      width: '100vw',
+      height: 'calc(100vh - 65px)',
+      right: '0',
+      top: '65px',
+    },
   },
   landingView__img: {
     position: 'absolute',
